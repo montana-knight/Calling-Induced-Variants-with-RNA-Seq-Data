c78d66ef769eb5f50a4265b5a7c297d1e62e18be
montana-knight <68970630+montana-knight@users.noreply.github.com>
2020-10-15 12:59:00 -0400

    Update trim_fastq.sh

--- step0/scripts/trim_fastq.sh
+++ step0/scripts/trim_fastq.sh
@@ -4,3 +4,6 @@ fastx_trimmer -Q33 -f ## -l ## -i input_raw.fastq -o trimmed.fastq
 
 # the -Q33 tag might need to be added depending on the sequencer that you used to get your reads
 # there are many other tools you can use to trim your reads
+# the -f tag is the "f"irst number of nucleotides that need to be cut off the reads
+# the -l tag is the "l"ast number of nucleotides that should be cut off
+
