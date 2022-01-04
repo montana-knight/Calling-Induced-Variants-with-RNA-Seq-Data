a845c2531b85188c9f5421de42d3404ab865cba0
montana-knight <68970630+montana-knight@users.noreply.github.com>
2022-01-04 14:58:54 -0500

    Update part2.a.genomicsdbimport.sh

--- step2/scripts/ultimatePON/part2.a.genomicsdbimport.sh
+++ step2/scripts/ultimatePON/part2.a.genomicsdbimport.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-gatk GenomicsDBImport -R genome.fasta -L intervals.list --genomicsdb-workspace-path hapcall_pon_dir --sample-name-map samplename.map
+gatk GenomicsDBImport -R genome.fasta -L intervals.list --genomicsdb-workspace-path workspace_dir_name --sample-name-map mappingfile.map
+
+### 
+
+# -R reference genome
+# -L intervals.list (intervals in genomes to search over)
+# --genomicsdb-workspace-path workspace_dir_name (where you want the workspace to be/what you want it called
+# --sample-name-map mappingfile.map (your samples/where to find them)
+
+# https://gatk.broadinstitute.org/hc/en-us/articles/360036883491-GenomicsDBImport
+
+# you will need to use this tool on both your haplotypecaller vcfs and your mutect2 vcfs (separately)
+
