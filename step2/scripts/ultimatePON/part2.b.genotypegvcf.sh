5763bd48ffa31fe5a7243ffeee58bc2433759cde
montana-knight <68970630+montana-knight@users.noreply.github.com>
2022-01-05 12:48:22 -0500

    Update part2.b.genotypegvcf.sh

--- step2/scripts/ultimatePON/part2.b.genotypegvcf.sh
+++ step2/scripts/ultimatePON/part2.b.genotypegvcf.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-gatk GenotypeGVCFs --R genome.fasta -V gendb://hapcall_pon_dir -O output.vcf
+gatk GenotypeGVCFs --R genome.fasta -V gendb://workspace_dir_name -O output.vcf
+
+# the genotypegvcfs tool is for the vcf files from haplotype caller. you need to use this tool after you create a genomicdb with
+# those files (see the genomicsdbimport script).
+
+# this step is needed to perform JOINT genotyping (ie, what are the germline mutations among the samples, as called by haplotype caller)
+
+# https://gatk.broadinstitute.org/hc/en-us/articles/360037057852-GenotypeGVCFs
+
+# you will need to also run the "create somatic panel of normals" tool after this
