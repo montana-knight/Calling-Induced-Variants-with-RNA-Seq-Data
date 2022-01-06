1937d62a446ef06fb8c832d5309ec0a4a9702921
montana-knight <68970630+montana-knight@users.noreply.github.com>
2022-01-06 12:02:02 -0500

    Update part2.e.finalediting.sh

--- step2/scripts/ultimatePON/part2.e.finalediting.sh
+++ step2/scripts/ultimatePON/part2.e.finalediting.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# The end of this set of scripts will give you an "Ultimate Panel of Normals" from the final R script. It will be missing the 
+# normal vcf header so you will need to attach that from another file. For example you can pull out all of the header 
+# by doing something like this:
 
-# The end of this set of scripts will give you an "Ultimate Panel of Normals" from the final R script. It will be missing the normal vcf header so you will need to attach that from another file. For example you can pull out all of the header by doing something like this:
+grep "#" somevcffile.vcf >header.txt
 
->   grep "#" somevcffile.vcf >header.txt
+# Then attach that header to your ultimate pon and create a vcf version of you file.
 
-Then attach that header to your ultimate pon and create a vcf version of you file.
+cat header.txt ultimatepon.tsv >ultimatepon.vcf # ultimatepon.tsv is the ultimate pon from the r script
 
->   cat header.txt ultimatepon.tsv >ultimatepon.vcf
+# you will also need to index it, this is necessary for any future run of any GATK tool
+
+gatk IndexFeatureFile -I ultimatePON.vcf
+
+# https://gatk.broadinstitute.org/hc/en-us/articles/360037428111-IndexFeatureFile
