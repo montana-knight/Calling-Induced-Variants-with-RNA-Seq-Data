a649a0cab78dc0ee2c38401bb1318a4bc677ea55
montana-knight <68970630+montana-knight@users.noreply.github.com>
2022-09-07 12:04:13 -0400

    Update part1.a.haplotypecaller.sh

--- step2/scripts/ultimatePON/part1.a.haplotypecaller.sh
+++ step2/scripts/ultimatePON/part1.a.haplotypecaller.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-gatk HaplotypeCaller --input bam_filename.bam --output output_filename.g.vcf --reference genome.fasta --heterozygosity 0.0001 -ERC GVCF
+gatk HaplotypeCaller --input bam_filename.bam --output output_filename.g.vcf --reference genome.fasta -ERC GVCF
 
 # the input file here is the output file from the splitncigar reads tool
 # https://gatk.broadinstitute.org/hc/en-us/articles/360037225632-HaplotypeCaller
 
 # haplotype caller calls germline variants within your samples, perform on each sample
+
+# there is a parameter "heterozygosity" which should be set to a low value when working with an inbred species
