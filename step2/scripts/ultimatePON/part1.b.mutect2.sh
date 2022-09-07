5458dfaf2c68f9faf8a44049b94183cc3e51ca7a
montana-knight <68970630+montana-knight@users.noreply.github.com>
2022-09-07 12:05:17 -0400

    Update part1.b.mutect2.sh

--- step2/scripts/ultimatePON/part1.b.mutect2.sh
+++ step2/scripts/ultimatePON/part1.b.mutect2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-gatk Mutect2 -R genome.fasta --max-num-haplotypes-in-population 2 -I bam_filename.bam -max-mnp-distance 0 -O output_filename.vcf.gz
+gatk Mutect2 -R genome.fasta -I bam_filename.bam -O output_filename.vcf.gz
 
 # in reality this is run parallel to haplotype caller
 # the input file is the output file from te splitncigarreads tool
 # the .gz tag in the output file zips it up
 
 # https://gatk.broadinstitute.org/hc/en-us/articles/360037593851-Mutect2
+
+# the --max-num-haplotypes-in-population should be set to a low value to accomodate a highly inbred species
