be44476b58d3a9204cdc287b5374f7b1aafe45e1
montana-knight <68970630+montana-knight@users.noreply.github.com>
2022-09-07 12:13:39 -0400

    Update variantfiltration.sh

--- step4/scripts/variantfiltration.sh
+++ step4/scripts/variantfiltration.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-gatk VariantFiltration -V vcf_file.vcf -O mutect2/$filename.mutect.filtered.vcf -R /home3/mknight/SpaceRadiation/TAIR10_chr_all.fasta -window 35 -cluster 3 --filter-name DP -filter "DP < 10" --filter-name TLOD -filter "TLOD < 6.00"
+gatk VariantFiltration -V vcf_file.vcf -O hardfiltered.vcf -R genomereference.fasta 
+
+# now to apply your hard filters
+
+# -V the vcf output file from mutect2 that was created during the variant call step
+# -O your filtered vcf file output name
+# -R your reference genome
+
+## there are a lot of potential parameters you can filter on, see: https://gatk.broadinstitute.org/hc/en-us/articles/360037434691-VariantFiltration
+
+# I suggest these to start: -window 35 -cluster 3 --filter-name DP -filter "DP < 10" --filter-name TLOD -filter "TLOD < 6.00"
